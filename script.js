81f1e2fb4b46d6378fd180eeb3c201b29f9a7960
Maryna Zeinalova <shmapetto@gmail.com>
2023-02-20 13:06:28 +0200

    added text without variables

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ note.innerHTML = `
 </p>
 <p>Thank you for your understanding.</p>
 <p>Sincerely,<br />${parentName} <br />(${parentGender})</p>`
+
+// let note = document.querySelector('.note-content');
+// note.innerHTML = `
+// <p id="date-element">20.02.2023</p>
+// <p>Dear <span id="teacher-gender-element">Mr.</span> <span id="teacher-name-element">Cooper</span>,</p>
+// <p>
+//   Please excuse my <span id="child-gender-element">son</span>, <span id="child-name-element">Leonard Hofstadter</span> for being absent from
+//   school on <span id="absence-date-element">20.02.2023</span> due to the <span id="reason-element">family reasons</span>.
+// </p>
+// <p>Thank you for your understanding.</p>
+// <p>Sincerely,<br /> <span id="parent-name-element">Beverly Hofstadter</span> <br />(<span id="parent-gender-element">mother</span>)</p>`
